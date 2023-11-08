4df006bb40ddbb2230b0abaffc84ccc7a6ab2537
Camilo Jimenez <camiloj896@gmail.com>
2023-11-08 07:28:22 -0500

    :hammer: feat: add modal to show answer

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
-import { useState } from 'react'
+import { useState, Fragment } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
 
 import './App.css'
 
 function App() {
   const [creditos, setCreditos] = useState(false)
+  const [modal, setModal] = useState('');
+  const [open, setOpen] = useState(false);
+
 
   const handleSubmit = async (e) => {
     e.preventDefault()
@@ -43,9 +47,13 @@ function App() {
       body: JSON.stringify(data2),
     });
     
-    const responde = await response.json();
+    const res = await response.json();
 
-    console.log(responde)
+    const modalMessage = res.response === 1 ? 'Credito Aprobado' : 'Credito Denegado';
+    setModal(modalMessage);
+    setOpen(true);
+
+    document.forms[0].reset()
 
     return false;
   }
@@ -54,9 +62,7 @@ function App() {
     <section>
       <form onSubmit={handleSubmit}>
         <div className="border-b border-gray-900/10 pb-12">
-          <h2 className="text-base font-semibold leading-7 text-gray-900">Personal Information</h2>
-          <p className="mt-1 text-sm leading-6 text-gray-600">Use a permanent address where you can receive mail.</p>
-
+          <h2 className="text-base font-semibold leading-7 text-gray-900">Simulador De Credito</h2>
           <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
             <div className="sm:col-span-3">
               <label htmlFor="document_number" className="block text-sm font-medium leading-6 text-gray-900">
@@ -240,6 +246,56 @@ function App() {
           <button type="submit" className="flex w-full justify-center rounded-md bg-orange-500 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-900 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600">Sign in</button>
         </div>
       </form>
+      <Transition.Root show={open} as={Fragment}>
+      <Dialog as="div" className="relative z-10" onClose={setOpen}>
+        <Transition.Child
+          as={Fragment}
+          enter="ease-out duration-300"
+          enterFrom="opacity-0"
+          enterTo="opacity-100"
+          leave="ease-in duration-200"
+          leaveFrom="opacity-100"
+          leaveTo="opacity-0"
+        >
+          <div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+        </Transition.Child>
+
+        <div className="fixed inset-0 z-10 w-screen overflow-y-auto">
+          <div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+            <Transition.Child
+              as={Fragment}
+              enter="ease-out duration-300"
+              enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+              enterTo="opacity-100 translate-y-0 sm:scale-100"
+              leave="ease-in duration-200"
+              leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+              leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+            >
+              <Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg">
+                <div className="bg-white px-4 pb-4 pt-5 sm:p-6 sm:pb-4">
+                  <div className="sm:flex sm:items-start">
+                    <div className="mt-3 text-center sm:ml-4 sm:mt-0 sm:text-left">
+                      <Dialog.Title as="h3" className="text-base font-semibold leading-6 text-gray-900">
+                        {modal}
+                      </Dialog.Title>
+                    </div>
+                  </div>
+                </div>
+                <div className="bg-gray-50 px-4 py-3 sm:flex sm:flex-row-reverse sm:px-6">
+                  <button
+                    type="button"
+                    className="mt-3 inline-flex w-full justify-center rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 sm:mt-0 sm:w-auto"
+                    onClick={() => setOpen(false)}
+                  >
+                    Cerrar
+                  </button>
+                </div>
+              </Dialog.Panel>
+            </Transition.Child>
+          </div>
+        </div>
+      </Dialog>
+    </Transition.Root>
     </section>
   )
 }
